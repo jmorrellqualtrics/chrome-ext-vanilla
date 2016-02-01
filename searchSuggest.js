e98a0638ff66e4fb8d3d2c41af875f7c4b4108c3
jmorrell <jmorrell@qualtrics.com>
2016-02-01 14:03:08 -0700

    fixed bug that prevented search terms from disappearing

--- searchSuggest.js
+++ searchSuggest.js
@@ -17,22 +17,22 @@ $( document ).ready(function() {
 
 	// Search Suggest Functions
 	function searchSuggest() {
-		// Choose a Random Number
-		var happenstance = randomIntFromInterval(0,14)
-		// If modulus of random number is 2 - Roughly 33% of the time
-		if (happenstance % 3 == 2) {
-			// Select a random topic
-			var topicIndex = chooseTopicIndex();
-			// If random topic has already been suggested
-			if ( $(".searchSuggestBox").length ) {
-				// Remove it
-				$(".searchSuggestBox").remove();
-			// If randome topic has not been suggested
-			} else {
+		// If random topic has already been suggested
+		if ( $(".searchSuggestBox").length ) {
+			// Remove it
+			$(".searchSuggestBox").remove();
+		// If randome topic has not been suggested
+		} else {
+			// Choose a Random Number
+			var happenstance = randomIntFromInterval(0,14)
+			// If modulus of random number is 2 - Roughly 33% of the time
+			if (happenstance % 3 == 2) {
+				// Select a random topic
+				var topicIndex = chooseTopicIndex();
 				// Suggest it
 				$("#center_col").prepend('<div class="searchSuggestBox" style="padding:0 8px"><div class="med"><div><p class="ssp card-section"><span class="spell _uwb">Did you mean:</span> <a class="spell" href="' + searchURL[topicIndex] + '"><b><i>' + searchTerm[topicIndex] + '</i></b></a> </p></div><hr class="rgsep _l4"></div></div>');
-			}				
-		}
+			}
+		}				
 	}
 
 	// Helper Functions
